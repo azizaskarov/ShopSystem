53b9ada5cddebe824909d7011c3db0c02d137116
azizaskarow <azizasqarov773@gmail.com>
2023-09-26 13:49:22 +0500

    Add categroy button

--- WpfForM-CRM/Pages/ChildCategory/ChildCategoryControl.xaml.cs
+++ WpfForM-CRM/Pages/ChildCategory/ChildCategoryControl.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfForM_CRM.Context;
+using WpfForM_CRM.Pages.Shop;
 
 namespace WpfForM_CRM.Pages.ChildCategory
 {
@@ -20,11 +22,15 @@ namespace WpfForM_CRM.Pages.ChildCategory
     /// </summary>
     public partial class ChildCategoryControl : UserControl
     {
-        public ChildCategoryControl()
+        public ChildCategoryControl(ShopsPage shopsPage)
         {
+            this.shopsPage = shopsPage;
+            this.appDbContext = new AppDbContext();
             InitializeComponent();
         }
 
+        private ShopsPage shopsPage;
+        private AppDbContext appDbContext;
         public object ChildCategoryId
         {
             get
@@ -33,7 +39,7 @@ namespace WpfForM_CRM.Pages.ChildCategory
             }
             set
             {
-                childCategoryId.Content = value.ToString();
+                childCategoryId.Content = value;
             }
         }
 
@@ -65,12 +71,30 @@ namespace WpfForM_CRM.Pages.ChildCategory
 
         private void ChildCategoryNameDelete_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            throw new NotImplementedException();
+
+
+            var childCategory = appDbContext.ChildCategories
+                .FirstOrDefault(ch => ch.Id == (Guid?)ChildCategoryId);
+            var deleteResultButton = MessageBox.Show("Вы уверены, что хотите удалить под категория?",
+                "Удалит",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+
+            if (deleteResultButton == MessageBoxResult.Yes)
+            {
+
+                appDbContext.ChildCategories.Remove(childCategory);
+                appDbContext.SaveChanges();
+                shopsPage.ReadChildCategories();
+            }
+
         }
 
         private void ChildCategoryNameUpdateImage_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            throw new NotImplementedException();
+            var updateChildCategory =
+                new UpdateChildCategory(shopsPage, (Guid)ChildCategoryId, (string)ChildCategoryName, CategoryId);
+            updateChildCategory.ShowDialog();
         }
     }
 }
