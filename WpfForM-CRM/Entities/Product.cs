50faa6670fb7b26ff1a9e9ae41aace5038821651
azizaskarow <azizasqarov773@gmail.com>
2023-09-27 17:49:45 +0500

    Edit product model

--- WpfForM-CRM/Entities/Product.cs
+++ WpfForM-CRM/Entities/Product.cs
@@ -8,14 +8,15 @@ public class Product
     [Key]
     public Guid Id { get; set; }
     public required string Name { get; set; }
-    public string? Description { get; set; }
     public DateTime? CreatedDate { get; set; } = DateTime.UtcNow;
-    public long? Price { get; set; }
+    public long? OriginalPrice { get; set; }
+    public long? SellingPrice { get; set; }
+    public int? Count { get; set; }
+    public string? Barcode { get; set; }
 
     public Guid? ChildCategoryId { get; set; }
     public ChildCategory? ChildCategory { get; set; }
 
-    
     public Guid? ShopId { get; set; }
     public Guid? CategoryId { get; set;}
     public Guid? UserId { get; set; }
---
+++ WpfForM-CRM/Migrations/20230927115433_AddproductEntityUpdate2.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WpfForM_CRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddproductEntityUpdate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Products",
+                newName: "SellingPrice");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Products",
+                newName: "Barcode");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "OriginalPrice",
+                table: "Products",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalPrice",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "SellingPrice",
+                table: "Products",
+                newName: "Price");
+
+            migrationBuilder.RenameColumn(
+                name: "Barcode",
+                table: "Products",
+                newName: "Description");
+        }
+    }
+}
