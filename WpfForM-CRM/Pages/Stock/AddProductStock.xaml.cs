a3c22957635d91e4c33353122ee32d9f58d83114
azizaskarow <azizasqarov773@gmail.com>
2023-09-29 15:35:33 +0500

    Added

--- WpfForM-CRM/Pages/Stock/AddProductStock.xaml.cs
+++ WpfForM-CRM/Pages/Stock/AddProductStock.xaml.cs
@@ -1,27 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace WpfForM_CRM.Pages.Stock
+namespace WpfForM_CRM.Pages.Stock;
+
+/// <summary>
+/// Interaction logic for AddProductStock.xaml
+/// </summary>
+public partial class AddProductStock : Window
 {
-    /// <summary>
-    /// Interaction logic for AddProductStock.xaml
-    /// </summary>
-    public partial class AddProductStock : Window
+
+    public AddProductStock()
     {
-        public AddProductStock()
+        InitializeComponent();
+    }
+
+    private void ProductName_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        string pattern = @"^[а-яА-Яa-zA-Z0-9]+$";
+
+        // Create a regular expression object with the pattern
+        Regex regex = new Regex(pattern);
+
+        // Check if the entered text matches the pattern
+        if (!regex.IsMatch(e.Text))
         {
-            InitializeComponent();
+            e.Handled = true; // Ignore the input
+        }
+    }
+
+    private void ProductPrice_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+
+        string pattern = @"^[0-9]+$";
+
+        // Create a regular expression object with the pattern
+        Regex regex = new Regex(pattern);
+
+        // Check if the entered text matches the pattern
+        if (!regex.IsMatch(e.Text))
+        {
+            e.Handled = true; // Ignore the input
+        }
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void category_name_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void subcategory_name_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (categoryNameComboBox.SelectedValue == null)
+        {
+            MessageBox.Show("Category not selected");
+            return;
         }
     }
 }
+
+
