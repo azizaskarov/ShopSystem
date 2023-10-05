e621ca891b017a2bcf06bb16f160f244c02a3568
azizaskarow <azizasqarov773@gmail.com>
2023-10-05 16:44:59 +0500

    Code Rub

---
+++ WpfForM-CRM/Pages/CashRegister/AddCashRegisterWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfForM_CRM.Pages.CashRegister;
+
+/// <summary>
+/// Interaction logic for AddCashRegisterWindow.xaml
+/// </summary>
+public partial class AddCashRegisterWindow : Window
+{
+    public AddCashRegisterWindow()
+    {
+        InitializeComponent();
+        Focus();
+    }
+}
---
+++ WpfForM-CRM/Pages/CashRegister/CashRegisterPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using WpfForM_CRM.Context;
+using WpfForM_CRM.Pages.Shop;
+
+namespace WpfForM_CRM.Pages.CashRegister
+{
+    /// <summary>
+    /// Interaction logic for CashRegisterPage.xaml
+    /// </summary>
+    public partial class CashRegisterPage : Page
+    {
+        public CashRegisterPage(ShopsPage shopsPage)
+        {
+           
+            InitializeComponent();
+            ReadCashRegisterControls();
+            this.shopsPage = shopsPage;
+            shopName.Text = "Магазин: " + shopsPage.ShopName;
+        }
+
+        ShopsPage shopsPage;
+
+        public void ReadCashRegisterControls()
+        {
+            var db = new AppDbContext();
+            var names = new List<string>()
+            {
+                "Kassa1",
+                "Kassa2",
+                "Kassa3"
+            };
+
+            var cashRegisterControls = new List<CashRegisterControl>();
+
+            foreach (var name in names)
+            {
+                var cashRegisterControl = new CashRegisterControl
+                {
+                    CashRegisterName = name
+                };
+                cashRegisterControls.Add(cashRegisterControl);
+            }
+
+            cashRegisterFrame.ItemsSource = cashRegisterControls;
+        }
+
+        private void Button_Add(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
